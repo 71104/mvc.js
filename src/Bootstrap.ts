eec8fb18d5e91fd0e0cf4ea84603aa4852ef254f
71104 <a71104@gmail.com>
2020-01-04 19:40:37 +0000

    Automatically register templates from <template> elements.

--- src/Bootstrap.ts
+++ src/Bootstrap.ts
@@ -4,14 +4,32 @@
 namespace MVC {
 
 
-function _bindDefault() {
-  window.removeEventListener('DOMContentLoaded', _bindDefault, false);
+function _registerTemplates() {
+  const elements = document.querySelectorAll('template');
+  Array.from(elements).forEach(element => {
+    try {
+      MVC.Templates.register(element.id, element.content);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+}
+
+
+function _bootstrap() {
   const elements = document.querySelectorAll('[mvc-app]');
   if (elements.length > 1) {
     throw new Error(`${elements.length} elements tagged with 'mvc-app' have been detected (there can be only 1)`);
   } else if (elements.length > 0) {
     MVC.bind({}, elements[0]);
   }
+}
+
+
+function _bindDefault() {
+  window.removeEventListener('DOMContentLoaded', _bindDefault, false);
+  _registerTemplates();
+  _bootstrap();
 }
 
 window.addEventListener('DOMContentLoaded', _bindDefault, false);
