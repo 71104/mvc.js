14135f366ab2fe1446e07d5995cd3ffb663894f0
71104 <a71104@gmail.com>
2020-01-20 18:34:56 +0000

    Enforce component name format

--- src/Components.ts
+++ src/Components.ts
@@ -8,35 +8,42 @@ export namespace Components {
 const _REGISTRY: {[name: string]: string} = Object.create(null);
 
 
-export function register(name: string, templateName: string): void {
+function _getCanonicalName(name: string): string {
   const canonicalName = name.toLowerCase();
+  if (/^[a-z]([a-z-]*[a-z])?$/.test(canonicalName)) {
+    return canonicalName;
+  } else {
+    throw new Error(`invalid component name: ${JSON.stringify(name)}`);
+  }
+}
+
+
+function _checkName(name: string): string {
+  const canonicalName = _getCanonicalName(name);
   if (canonicalName in _REGISTRY) {
     throw new Error(`double registration for template "${canonicalName}"`);
   } else {
-    _REGISTRY[canonicalName] = templateName;
+    return canonicalName;
   }
+}
+
+
+export function register(name: string, templateName: string): void {
+  _REGISTRY[_checkName(name)] = templateName;
 }
 
 
 export function registerFromString(name: string, template: string): void {
-  const canonicalName = name.toLowerCase();
-  if (canonicalName in _REGISTRY) {
-    throw new Error(`double registration for template "${canonicalName}"`);
-  } else {
-    MVC.Templates.registerFromString(canonicalName, template);
-    _REGISTRY[canonicalName] = canonicalName;
-  }
+  const canonicalName = _checkName(name);
+  MVC.Templates.registerFromString(canonicalName, template);
+  _REGISTRY[canonicalName] = canonicalName;
 }
 
 
 export function registerTrimmedString(name: string, template: string): void {
-  const canonicalName = name.toLowerCase();
-  if (canonicalName in _REGISTRY) {
-    throw new Error(`double registration for template "${canonicalName}"`);
-  } else {
-    MVC.Templates.registerTrimmedString(canonicalName, template);
-    _REGISTRY[canonicalName] = canonicalName;
-  }
+  const canonicalName = _checkName(name);
+  MVC.Templates.registerTrimmedString(canonicalName, template);
+  _REGISTRY[canonicalName] = canonicalName;
 }
 
 
@@ -46,7 +53,7 @@ export function isValid(name: string): boolean {
 
 
 export function lookupTemplate(name: string): DocumentFragment {
-  const canonicalName = name.toLowerCase();
+  const canonicalName = _getCanonicalName(name);
   if (canonicalName in _REGISTRY) {
     return MVC.Templates.lookup(_REGISTRY[canonicalName]);
   } else {
